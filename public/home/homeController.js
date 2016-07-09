669c5ce8a7694507a167beed6127c89355a1083e
captcontrol <pridham.tom@gmail.com>
2016-07-08 18:34:32 -0600

    structured immunization data

--- public/home/homeController.js
+++ public/home/homeController.js
@@ -1,20 +1,39 @@
 `use strict`;
-import  Chart  from 'Chart'
+import  Chart  from 'chart.js'
 import { keys } from '../../keys.js';
 
 module.exports = function ($scope, $http, $q) {
 
+    Chart.defaults.global = {
+        responsive: true,
+        maintainAspectRatio: true
+
+    };
     let ctx = document.getElementById("myChart");
     $scope.count = 0;
 
-    let data2 = [];
+    let data2 = {labels: {}, data:[]};
     let myChart;
 
     $http.get('https://opendata.utah.gov/resource/9heb-s7ea.json', keys).then(
         (req)=> {
+            console.log(Object.keys(data2.labels).length);
+            let index =0;
             req.data.forEach(datum => {
-                data2.push({"x": datum.adequately_immunized, "y": datum.school_district});
+                // debugger;
+                if (data2.labels[datum.school_district] === undefined) {
+                    console.log(Object.keys(data2.labels).length);
+                    index = Object.keys(data2.labels).length;
+                    data2.labels[datum.school_district] = index;
+                    data2.data.push([Number(datum.of_students_adequately_immunized_all_six_vaccines), Number(datum.total_students_enrolled)]);
+                } else{
+                    index = data2.labels[datum.school_district];
+                    data2.data[index] = [data2.data[index][0] + Number(datum.of_students_adequately_immunized_all_six_vaccines), data2.data[index][1] + Number(datum.total_students_enrolled)];
+                }
             });
+
+            // data2.forEach
+            console.log(data2)
 
         }, (req, res)=> {
         });
@@ -24,10 +43,10 @@ module.exports = function ($scope, $http, $q) {
         myChart = new Chart(ctx, {
             type: 'bar',
             data: {
-                labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+                labels: data2.districts,
                 datasets: [{
                     label: '# of Votes',
-                    data: [12, 19, 3, 5, 2, 3],
+                    data: [],
                     backgroundColor: [
                         'rgba(255, 99, 132, 0.2)',
                         'rgba(54, 162, 235, 0.2)',
